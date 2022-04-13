e2559f2543234db255beffa32b862604d1d78c34
vg26 <vlad.gulabovski@gmail.com>
2022-04-14 01:42:21 +0200

    JS-Exercise-2

---
+++ js-intro-homework/js-exercise-2/js-exercise-2.js
@@ -0,0 +1,24 @@
+let radius = 10
+area = Math.PI*(radius*radius)
+console.log(`The area of a circle with radius of ${radius} is: ${area.toFixed(2)}`)
+
+console.log('--------------------------------------')
+
+let circleCircumference = 100
+radius = circleCircumference / (2*Math.PI)
+console.log(`The circle has a circumference of ${circleCircumference}. Its radius is ${radius.toFixed(1)}`)
+
+console.log('--------------------------------------')
+
+diametar = radius.toFixed(1)*2
+console.log(`The circle has a circumference of ${circleCircumference}m. Its radius is ${radius.toFixed(1)} and the diametar is ${diametar}`) 
+
+
+
+
+
+
+
+
+
+
