345cc2aa113550e9d55b51db474701ce13f523eb
vg26 <vlad.gulabovski@gmail.com>
2022-04-14 01:10:59 +0200

    JS-Exercise-1

---
+++ js-intro-homework/js-exercise-1/js-exercise.js
@@ -0,0 +1,29 @@
+const country = 'US'
+const city = 'Boston'
+const population = 600000
+
+//alert(`The selected country is: ${country}`)
+//alert(`My favourite city is: ${city}`)
+//alert(`The population is: ${population}`)
+
+console.log(`The selected country is: ${country}`)
+console.log(`My favourite city is: ${city}`)
+console.log(`The population is: ${population}`)
+
+console.log('--------------------------------')
+
+const capitalCity = 'Washington'
+const isCountryCapital = city === capitalCity
+console.log(`Is Country Capital: ${isCountryCapital}`)
+
+console.log('--------------------------------')
+
+const lastTimeVisited = '2008'
+let numberOfTimesVisited = 17
+console.log(`The last time I visited ${city} was in ${lastTimeVisited}. So far I have visited ${city} ${numberOfTimesVisited} times`)
+
+numberOfTimesVisited = numberOfTimesVisited + 1
+console.log(`The last time I visited ${city} was in ${lastTimeVisited}. So far I have visited ${city} ${numberOfTimesVisited} times`)
+
+console.log('--------------------------------')
+
