4fc33e3cdb0360ef89c0ab669ce4bf703146e7ac
vg26 <65300700+vg26@users.noreply.github.com>
2022-06-05 19:34:18 +0200

    JS-intro-es6 (#20)

---
+++ js-intro-es6/index.js
@@ -0,0 +1,23 @@
+//Exercise 1
+import { firstUser } from './constants.js';
+import convertToFullName from './helpers/exercise1_helper.js';
+
+//Exercise 2
+import { firstTeam, secondTeam, otherTeams } from './constants.js';
+
+//Exercise 3
+import { allFruits } from './constants.js';
+
+
+//Exercise 1
+const userFullName = convertToFullName(firstUser);
+console.log(userFullName);
+
+//Exercise 2
+console.log(firstTeam, secondTeam, otherTeams);
+
+//Exercise 3
+console.log(allFruits)
+
+
+
